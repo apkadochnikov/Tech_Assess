f4c87560f6e8de56218deab3695b5d500bcaabd3
APKadochnikov <112889961+apkadochnikov@users.noreply.github.com>
2024-04-26 05:15:58 +0300

    Create Run.sh

---
+++ 9/Run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+#Network settings
+docker network create traefik-network
+docker network create keycloak-network
+
+#Copy export bashrc
+cat 9/environment_variables >> ~/.bashrc
+source ~/.bashrc
+
+cd 9/
+docker compose -f docker_keycloak_postgresql_letsencrypt.yml -p keycloak up -d
